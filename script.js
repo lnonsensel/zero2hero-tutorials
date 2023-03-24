0012a2b004556ff4812adb7bf1467eb0f283976d
ltt <453273@gmail.com>
2023-03-24 18:25:09 +0300

    Added history, changed styles.css, added dynamic stake

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 //0xe2Ab25Cb5F1D40168B951e57582c786Df210F005  -With history
 //0x4e1433fb16f9be999d9f2c24e1fc5cacee9c5897 -Without history
 
-//const { ethers } = require("ethers");
-
 
 const contractAddress = "0xe2Ab25Cb5F1D40168B951e57582c786Df210F005"
 const abi = [
@@ -84,38 +82,58 @@ const provider = new ethers.providers.Web3Provider(window.ethereum, 97);
 
 let signer;
 let contract;
-var userWonl;
-var contractWonl;
-
+let usersChoice;
 
 provider.send("eth_requestAccounts", []).then(() => {
-    provider.listAccounts().then((accounts) => {
-        signer = provider.getSigner(accounts[0]);
-        contract = new ethers.Contract(contractAddress, abi, signer);
-        console.log(contract);
-    });
+	provider.listAccounts().then((accounts) => {
+		signer = provider.getSigner(accounts[0]);
+		contract = new ethers.Contract(contractAddress, abi, signer);
+		console.log(contract);
+	});
 });
 
-async function playGame(){
-    const usersChoice = document.getElementById("usersChoice").value;
-	contract.on("GamePlayed",
-	 (player, contractValue, userValue, userWon, contractWon) => {
-		if (userWon == contractWon){
-			document.getElementById("gameResult").innerText = 'Draw. Your funds will be sent back to you';
-		}
-		else if (userWon < contractWon){
-			document.getElementById("gameResult").innerText = 'You lost';
-		}
-		else{
-			document.getElementById("gameResult").innerText = 'You won and doubled your stake';
-		}
-	});
-    result = await contract.playGame(usersChoice, { value: ethers.utils.parseEther("0.0001")}).then(document.getElementById("gameResult").innerText = 'Transaction pending...');
+function getButtonRock() {
+	console.log('rock');
+	usersChoice = 0;
+}
 
-async function getHistory(){
-	await contract.getMyHistory().then((result) => console.log(ethers.utils.defaultAbiCoder.decode("string[]", result)))
-	
+function getButtonScissors() {
+	console.log('scissors');
+	usersChoice = 1;
 }
 
 
+function getButtonPaper() {
+	console.log('paper');
+	usersChoice = 2;
+}
+
+
+
+async function playGame() {
+	let stake = document.getElementById('stake').value;
+	contract.on("GamePlayed",
+		(player, contractValue, userValue, userWon, contractWon) => {
+			if (userWon == contractWon) {
+				document.getElementById("game-status").innerText = 'Draw. Your funds will be sent back to you';
+			}
+			else if (userWon < contractWon) {
+				document.getElementById("game-status").innerText = 'You lost';
+			}
+			else {
+				document.getElementById("game-status").innerText = 'You won and doubled your stake';
+			}
+		});
+	result = await contract.playGame(usersChoice, { value: ethers.utils.parseEther(stake) }).then(document.getElementById("game-status").innerText = 'Transaction pending...');
+}
+
+async function getHistory() {
+	await contract.getMyHistory().then((result) => {
+		result = result.slice(-5,);
+		document.getElementById('history1').innerText = result[4];
+		document.getElementById('history2').innerText = result[3];
+		document.getElementById('history3').innerText = result[2];
+		document.getElementById('history4').innerText = result[1];
+		document.getElementById('history5').innerText = result[0];
+	});
 }
